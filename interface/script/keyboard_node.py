0bce8ac5aedcedc1f78f97a531da7a09055483f9
theus6 <msantossilva005@gmail.com>
2022-10-25 23:24:24 -0300

    teleop added

---
+++ interface/script/keyboard_node.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+from keyboard_interface.msg import Keys
+from keyboard_interface.msg import KeyEvent
+
+try:
+  from pynput.keyboard import Listener
+except ImportError as e:
+  import pip
+  print("pynput package is required. Installing...")
+  pip.main(['install', 'pynput'])
+  from pynput.keyboard import Listener
+
+
+class KeyboardInterface(Node):
+
+  def __init__(self):
+    super().__init__('keyboard_node')
+    self.keys_publisher_ = self.create_publisher(Keys, '/Keys', 10)
+    self.key_event_publisher_ = self.create_publisher(KeyEvent, '/KeyEvent', 10)
+
+    self.sticky_buttons = self.declare_parameter('sticky_buttons', False).get_parameter_value().bool_value
+    self.keys_publish_rate = self.declare_parameter('keys_publish_rate', 10.0).get_parameter_value().double_value
+
+    self.keys_msg = Keys()
+    self.timer = self.create_timer(1/self.keys_publish_rate, self.timerCallback)
+    self.get_logger().info("Initializing keyboard listener...")
+    listener = Listener(on_press=self.onPress, on_release=self.onRelease)
+    listener.start()
+  
+  def timerCallback(self):
+    self.keys_publisher_.publish(self.keys_msg)
+
+  def onRelease(self, key):
+    key = str(key).upper().replace("'", "")
+    key_event_msg = KeyEvent()
+    key_event_msg.event = KeyEvent.RELEASED
+    key_event_msg.key = key
+    self.key_event_publisher_.publish(key_event_msg)
+
+    if not self.sticky_buttons:
+      if key in self.keys_msg.pressed_keys:
+        self.keys_msg.pressed_keys.remove(key)
+
+  def onPress(self, key):
+    key = str(key).upper().replace("'", "")
+    key_event_msg = KeyEvent()
+    key_event_msg.event = KeyEvent.PRESSED
+    key_event_msg.key = key
+    self.key_event_publisher_.publish(key_event_msg)
+
+    if key not in self.keys_msg.pressed_keys:
+      self.keys_msg.pressed_keys.append(key)
+    elif self.sticky_buttons:
+      self.keys_msg.pressed_keys.remove(key)
+
+
+def main(args=None):
+  rclpy.init(args=args)
+
+  keyboard_interface = KeyboardInterface()
+
+  rclpy.spin(keyboard_interface)
+
+  keyboard_interface.destroy_node()
+  rclpy.shutdown()
+
+if __name__ == '__main__':
+  main()
---
+++ teleop/src/JoyTeleop.cpp
@@ -0,0 +1,55 @@
+#include <memory>
+#include <chrono>
+
+#include "rclcpp/rclcpp.hpp"
+#include "joy_teleop/JoyTeleop.hpp"
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+JoyTeleop::JoyTeleop() : Node("joy_teleop"){
+    RCLCPP_INFO(this->get_logger(), "Joy Teleop node initialized");
+
+    _vel_publisher = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    _joy_subscriber = this->create_subscription<sensor_msgs::msg::Joy>("/joy", 10,std::bind(&JoyTeleop::joyCallback, this, _1));
+
+    _timer = this->create_wall_timer( 50ms, std::bind(&JoyTeleop::publishVelocity, this) );
+
+    _stop1       = this->declare_parameter<int>("stop_button1",   4);
+    _linear_x    = this->declare_parameter<int>("axis_linear_x",  1);
+    _linear_y    = this->declare_parameter<int>("axis_linear_y",  0);
+    _angular     = this->declare_parameter<int>("axis_angular",   3);
+    _lin_scale   = this->declare_parameter<double>("scale_linear",  1.0);
+    _ang_scale   = this->declare_parameter<double>("scale_angular", 1.0);
+
+    _rev = true;
+}
+
+JoyTeleop::~JoyTeleop(){
+}
+
+void JoyTeleop::joyCallback(const sensor_msgs::msg::Joy::SharedPtr msg){
+  if(msg->buttons[_reverse]) _rev = !_rev;
+  
+  _k = 0.5;
+  if(msg->buttons[_stop1] || msg->buttons[_stop2]) _k = 0;
+  
+  _twist.linear.x = _k*_lin_scale*msg->axes[_linear_x];
+  _twist.linear.y = _k*_lin_scale*msg->axes[_linear_y];
+
+  if(msg->axes[_linear_x] < 0 && _rev) _k = -_k;
+  _twist.angular.z = _k*_ang_scale*msg->axes[_angular];
+
+}
+
+void JoyTeleop::publishVelocity(){
+  _vel_publisher->publish(_twist);
+}
+
+
+int main(int argc, char **argv){
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<JoyTeleop>());
+  rclcpp::shutdown();
+  return 0;
+}
